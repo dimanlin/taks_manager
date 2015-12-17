bb7562af79854e0b06f8c17f56521b099b62e4c9
Dmitry Khromov <devandart@ya.ru>
2015-12-17 16:58:53 +0300

    auth by token and remove empty files(helpers)

--- app/controllers/dashboard/dashboard_controller.rb
+++ app/controllers/dashboard/dashboard_controller.rb
@@ -1,5 +1,15 @@
 class Dashboard::DashboardController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :auth_by_token
   before_action :authenticate_user!
+
   layout 'dashboard'
+
+  private
+
+  def auth_by_token
+    user = User.find_by_token(params[:token])
+    sign_in(:user, user) if user
+  end
 end
